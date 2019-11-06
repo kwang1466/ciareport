fb08ffa2cf743af9e8dd3a3ac44246d7cc660a47
Kevin Wang <SUP-KWA@SUPPORT-KWA.corp.castsoftware.com>
2019-11-06 15:16:39 +0800

    update README

--- tests/test_networkx.py
+++ tests/test_networkx.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('../packages')
 import networkx as nx
-# import networkx as nx
 
-
-nx.test()
+# nx.test()
+MG = nx.MultiGraph()
+MG.add_weighted_edges_from([(1, 2, 0.5), (1, 2, 0.75), (2, 3, 0.5)])
+print(dict(MG.degree(weight='weight')))
